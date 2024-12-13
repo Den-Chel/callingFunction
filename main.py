1620877d63f6c2a37dd007bac3af1ac9891303e7
Den-Chel <denis.chechulin.31@gmail.com>
2024-12-12 20:30:33 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Written by: Denis Chechulin
+Date: Oct 03, 2024
+"""
+
+def greetings(fname):
+  print(f"Hello {fname}. Nice to see you today!")
+
+#Call this function
+greetings('Alex')
+
+
+
